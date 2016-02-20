a69932aa0205dd00a72f9addda697e249440f950
plnreddi <plnreddi@gmail.com>
2016-02-20 11:29:32 +0530

    forms added

--- client/app/app.js
+++ client/app/app.js
@@ -13,33 +13,136 @@ angular.module('bifrostApp', [
   'ui.select',
   'ui.checkbox'
 ])
-  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider, formlyConfigProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     //$locationProvider.html5Mode(true);
-  }).run(function($rootScope, $state, formlyConfig) {
+
+    // adi check this
+    formlyConfigProvider.setType({
+        name: 'multi-checkbox',
+        templateUrl: 'app/templates/multi-checkbox.tpl.html',
+        wrapper: ['bootstrapLabel', 'bootstrapHasError']
+    });
+
+
+  })
+
+  .run(function($rootScope, $state, formlyConfig) {
 
     formlyConfig.extras.removeChromeAutoComplete = true;
+
     formlyConfig.setType({
-      name: 'async-ui-select',
-      extends: 'select',
-      templateUrl: 'app/templates/async-ui-select.tpl.html'
+        name: 'async-ui-select',
+        extends: 'select',
+        templateUrl: 'app/templates/async-ui-select.tpl.html'
     });
+
     formlyConfig.setType({
-      name: 'datepicker',
-              extends: 'input',
-      templateUrl: 'app/templates/datepicker.tpl.html'
+        name: 'datepicker',
+        extends: 'input',
+        templateUrl: 'app/templates/datepicker.tpl.html'
+    });
+
+    // adi check this
+    formlyConfig.setType({
+        name: 'radioinline',
+        extends: 'radio',
+        templateUrl: 'app/templates/inline-radio.html'
     });
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
-      var requireLogin = toState.data.requireLogin;
+        var requireLogin = toState.data.requireLogin;
 
-      if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
-        event.preventDefault();
-        $state.go('login');
-
-      }
+        if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
+            event.preventDefault();
+            $state.go('login');
+        }
     });
 
+
+
   });
+
+
+
+/*
+
+app.run(function(formlyConfig) {
+    var attributes = [
+        'date-disabled',
+        'custom-class',
+        'show-weeks',
+        'starting-day',
+        'init-date',
+        'min-mode',
+        'max-mode',
+        'format-day',
+        'format-month',
+        'format-year',
+        'format-day-header',
+        'format-day-title',
+        'format-month-title',
+        'year-range',
+        'shortcut-propagation',
+        'datepicker-popup',
+        'show-button-bar',
+        'current-text',
+        'clear-text',
+        'close-text',
+        'close-on-date-selection',
+        'datepicker-append-to-body'
+    ];
+
+    var bindings = [ 'datepicker-mode', 'min-date', 'max-date' ];
+
+    var ngModelAttrs = {};
+
+    angular.forEach(attributes, function(attr) {
+        ngModelAttrs[camelize(attr)] = {attribute: attr};
+    });
+
+    angular.forEach(bindings, function(binding) {
+        ngModelAttrs[camelize(binding)] = {bound: binding};
+    });
+
+    console.log(ngModelAttrs);
+
+    formlyConfig.setType({
+        name: 'datepicker',
+        templateUrl:  'datepicker.html',
+        wrapper: ['bootstrapLabel', 'bootstrapHasError'],
+        defaultOptions: {
+            ngModelAttrs: ngModelAttrs,
+            templateOptions: {
+                datepickerOptions: {
+                    format: 'MM.dd.yyyy',
+                    initDate: new Date()
+                }
+            }
+        },
+
+        controller: ['$scope', function ($scope) {
+            $scope.datepicker = {};
+
+            $scope.datepicker.opened = false;
+
+            $scope.datepicker.open = function ($event) {
+                $scope.datepicker.opened = !$scope.datepicker.opened;
+            };
+        }]
+    });
+
+    function camelize(string) {
+        string = string.replace(/[\-_\s]+(.)?/g, function(match, chr) {
+            return chr ? chr.toUpperCase() : '';
+        });
+        // Ensure 1st char is always lowercase
+        return string.replace(/^([A-Z])/, function(match, chr) {
+            return chr ? chr.toLowerCase() : '';
+        });
+    }
+});
+
+*/
--- client/app/settings/settings.controller.js
+++ client/app/settings/settings.controller.js
@@ -1,110 +1,223 @@
-'use strict';
+(function() {
 
-angular.module('bifrostApp')
-  .controller('SettingsCtrl', function(settings) {
-    this.settings = settings.getAllSettings();
+    'use strict';
 
-    var patientDetailsForm = [{
-      className: 'row',
-      fieldGroup: [{
-        className: 'col-md-6',
-        type: 'input',
-        key: 'firstName',
-        templateOptions: {
-          label: 'First Name'
-        }
-      }, {
-        className: 'col-md-6',
-        type: 'input',
-        key: 'lastName',
-        templateOptions: {
-          label: 'Last Name'
-        },
-        expressionProperties: {
-          'templateOptions.disabled': '!model.firstName'
-        }
-      }]
-    }];
+    angular.module('bifrostApp')
+        .controller('SettingsCtrl', function(settings, patientData, practiceData, appointmentData) {
 
-    var clinicDetailsForm = [{
-      className: 'row',
-      fieldGroup: [{
-        className: 'col-md-6',
-        type: 'input',
-        key: 'firstName',
-        templateOptions: {
-          label: 'First Name'
-        }
-      }, {
-        className: 'col-md-6',
-        type: 'input',
-        key: 'lastName',
-        templateOptions: {
-          label: 'Last Name'
-        },
-        expressionProperties: {
-          'templateOptions.disabled': '!model.firstName'
-        }
-      }]
-    }, {
-      template: '<hr /><div><strong>Address:</strong></div>'
-    }, {
-      className: 'row',
-      fieldGroup: [{
-        className: 'col-md-4',
-        type: 'input',
-        key: 'street',
-        templateOptions: {
-          label: 'Street'
-        }
-      }, {
-        className: 'col-md-4',
-        type: 'input',
-        key: 'cityName',
-        templateOptions: {
-          label: 'City'
-        }
-      }, {
-        className: 'col-md-4',
-        type: 'input',
-        key: 'zip',
-        templateOptions: {
-          type: 'number',
-          label: 'Zip',
-          max: 99999,
-          min: 0,
-          pattern: '\\d{5}'
-        }
-      }]
-    }, {
-      template: '<hr />'
-    }, {
-      type: 'input',
-      key: 'otherInput',
-      templateOptions: {
-        label: 'Other Input'
-      }
-    }, {
-      type: 'checkbox',
-      key: 'otherToo',
-      templateOptions: {
-        label: 'Other Checkbox'
-      }
-    }];
+        var vm = this;
 
-    this.settingsForms = [{
-      title: 'form1',
-      form: clinicDetailsForm
-    }, {
-      title: 'form2',
-      form: patientDetailsForm
-    }, {
-      title: 'form3'
-    }];
+        vm.settings = settings.getAllSettings();
+   
+        vm.settingsForms = [
+            {
+                title: 'New Patient Registration'
+            }, 
+            {
+                title: 'Practice'
+            }, 
+            {
+                title: 'Appointment'
+            }
+        ];
 
-    this.changeForm = function(index) {
-      this.settingsFields = this.settingsForms[index].form;
+        vm.changeForm = function(index) {
+            switch (index) {
+                case 0:
+                    vm.loadingData = patientData.getModelFields().then(function(result) {
+                        vm.model = result[0];
+                        vm.fields1 = result[1];
+                        vm.fields2 = result[2];
+                        vm.fields3 = result[3];
+                        vm.fields4 = result[4];
+                        vm.originalFields = angular.copy(result);
+                    });
+                    break; 
 
-    };
+                case 1:
+                    vm.loadingData = practiceData.getModelFields().then(function(result) {
+                        vm.model = result[0];
+                        vm.fields1 = result[1];
+                        vm.fields2 = {};     
+                        vm.originalFields = angular.copy(result);
+                    });
+                    break; 
 
-  });
+                default: 
+                    vm.loadingData = appointmentData.getModalFields().then(function(result) {
+                        vm.model = result[0];
+                        vm.fields1 = result[1];
+                        vm.fields2 = result[2];
+                    //  vm.fields2 = result[3];
+                        vm.originalFields = angular.copy(result);
+                    });
+            }
+        };
+
+    });
+
+})();
+
+
+
+/*
+(function() {
+  
+    'use strict';
+
+    angular
+        .module('ngfrm1App')
+        .controller('PatientCtrl', function (patientData) {
+
+        var vm = this;
+
+        vm.onSubmit = onSubmit;
+       
+        vm.loadingData = patientData.getModelFields().then(function(result) {
+            vm.model = result[0];
+            vm.patientFields = result[1];
+            vm.medicalHistoryFields = result[2];
+            vm.originalFields = angular.copy(result);
+        });
+
+        function onSubmit() {
+            alert(JSON.stringify(vm.model), null, 2);
+        }
+
+    });
+  
+})();
+
+*/ /*
+        var patientDetailsForm = [{
+          className: 'row',
+          fieldGroup: [{
+            className: 'col-md-6',
+            type: 'input',
+            key: 'firstName',
+            templateOptions: {
+              label: 'First Name'
+            }
+          }, {
+            className: 'col-md-6',
+            type: 'input',
+            key: 'lastName',
+            templateOptions: {
+              label: 'Last Name'
+            },
+            expressionProperties: {
+              'templateOptions.disabled': '!model.firstName'
+            }
+          }]
+        }];
+
+        var clinicDetailsForm = [{
+          className: 'row',
+          fieldGroup: [{
+            className: 'col-md-6',
+            type: 'input',
+            key: 'firstName',
+            templateOptions: {
+              label: 'First Name'
+            }
+          }, {
+            className: 'col-md-6',
+            type: 'input',
+            key: 'lastName',
+            templateOptions: {
+              label: 'Last Name'
+            },
+            expressionProperties: {
+              'templateOptions.disabled': '!model.firstName'
+            }
+          }]
+        }, {
+          template: '<hr /><div><strong>Address:</strong></div>'
+        }, {
+          className: 'row',
+          fieldGroup: [{
+            className: 'col-md-4',
+            type: 'input',
+            key: 'street',
+            templateOptions: {
+              label: 'Street'
+            }
+          }, {
+            className: 'col-md-4',
+            type: 'input',
+            key: 'cityName',
+            templateOptions: {
+              label: 'City'
+            }
+          }, {
+            className: 'col-md-4',
+            type: 'input',
+            key: 'zip',
+            templateOptions: {
+              type: 'number',
+              label: 'Zip',
+              max: 99999,
+              min: 0,
+              pattern: '\\d{5}'
+            }
+          }]
+        }, {
+          template: '<hr />'
+        }, {
+          type: 'input',
+          key: 'otherInput',
+          templateOptions: {
+            label: 'Other Input'
+          }
+        }, {
+          type: 'checkbox',
+          key: 'otherToo',
+          templateOptions: {
+            label: 'Other Checkbox'
+          }
+        }];
+    
+
+        vm.sampleFields = [
+            {   type: 'input', key: 'patientName',
+                templateOptions: { label: 'Patient Name' }
+            },
+            
+            {   type: 'input', key: 'patientID',
+                templateOptions: { label: 'Patient ID' }
+            },
+
+            {   type: 'input', key: 'aadhaarID',
+                templateOptions: { label: 'Aadhaar ID' }
+            }
+        ];
+*/
+/*
+        vm.loadingData = patientData.getModelFields().then(function(result) {
+            vm.model = result[0];
+            vm.patientFields = result[1];
+            vm.medicalHistoryFields = result[2];
+            vm.originalFields = angular.copy(result);
+        
+      
+            vm.settingsForms = [
+                {
+                    title: 'New Patient Registration',
+                    form: vm.patientFields
+                }, 
+                {
+                    title: 'form2',
+                    form: vm.medicalHistoryFields
+                }, 
+                {
+                    title: 'form3'
+                }
+            ];
+
+            vm.changeForm = function(index) {
+                vm.settingsFields = vm.settingsForms[index].form;
+            };
+            
+        });
+
+*/
